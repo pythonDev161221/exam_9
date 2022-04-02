3a91c60f55c400a8106de1b598ac23195794a6ce
pythonDev161221 <adverbekreklamov@gmail.com>
2022-04-02 19:39:27 +0600

    add permission IsAuthenticated

--- source/api_v1/views.py
+++ source/api_v1/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.viewsets import ModelViewSet
 
 from api_v1.serializer import PhotoSerializer, UserSerializer
@@ -10,4 +11,5 @@ from webapp.models import Photo
 class PhotoViewSet(ModelViewSet):
     queryset = Photo.objects.all()
     serializer_class = PhotoSerializer
+    permission_classes = (IsAuthenticated, )
 
