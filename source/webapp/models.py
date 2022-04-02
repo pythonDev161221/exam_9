de2c55342ce7a2e2443407c43ceb45b1bd708a2d
pythonDev161221 <adverbekreklamov@gmail.com>
2022-04-02 14:53:20 +0600

    add some small improvements

--- source/webapp/models.py
+++ source/webapp/models.py
@@ -7,7 +7,7 @@ User = get_user_model()
 
 
 class Photo(models.Model):
-    photo = models.ImageField(upload_to='photos')
+    photo = models.ImageField(upload_to='photos', null=False, blank=False)
     signature = models.CharField(max_length=100)
     created_at = models.DateTimeField(auto_now_add=True)
     author = models.ForeignKey(User, related_name="photos", on_delete=models.CASCADE)
@@ -19,3 +19,4 @@ class Album(models.Model):
     description = models.TextField(max_length=200)
     author = models.ForeignKey(User, related_name='albums', on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
+
--- source/webapp/views/photos.py
+++ source/webapp/views/photos.py
@@ -18,15 +18,17 @@ class PhotoDetailView(DetailView):
     context_object_name = 'photo'
 
 
-class PhotoCreateView(LoginRequiredMixin, CreateView):
+class PhotoCreateView(CreateView):
     model = Photo
     template_name = 'photos/create_view.html'
     form_class = PhotoForm
+    print('asef')
 
-    def form_valid(self, form):
-        form.instance.author = self.request.user
-        self.object = form.save()
-        return super().form_valid()
+    # def form_valid(self, form):
+    #     form.instance.author = self.request.user
+    #     self.object = form.save()
+    #     print('valid')
+    #     return super().form_valid()
 
     def get_success_url(self):
         return reverse("webapp:photo_list_view")
