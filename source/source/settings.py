902cf6eea591ecbe46ea71cf45db53145d053683
pythonDev161221 <adverbekreklamov@gmail.com>
2022-04-02 10:59:16 +0600

    add base.html and index.html and webapp.urls

--- source/source/settings.py
+++ source/source/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'webapp',
+    'api_v1',
+    'accounts',
 ]
 
 MIDDLEWARE = [
